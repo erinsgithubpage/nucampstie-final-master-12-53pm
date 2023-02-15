469c16b619c3ce1df73f49224224c9ccc0622d8b
erinh <erin.hazelwood@outlook.com>
2023-02-15 05:14:01 -0500

    More RR, two - 3:00

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -4,7 +4,16 @@ import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 import CampsiteDetail from '../featuures/campsites/CampsiteDetail';
 
 const CampsiteDetailPage = () => {
+    const { campsiteId } = useParams();
+    const campsite = selectCampsiteById(campsiteId);
 
+    return (
+        <Container>
+            <Row>
+               <CampsiteDetail campsite={campsite} />
+            </Row>
+        </Container>
+    )
 };
 
 export default CampsiteDetailPage;
