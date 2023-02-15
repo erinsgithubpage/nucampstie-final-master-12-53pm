9379175337cfd34807a2f73655c91a95dd585f9c
erinh <erin.hazelwood@outlook.com>
2023-02-15 05:10:27 -0500

    More RR, two - 1:43

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -2,3 +2,9 @@ import { Container, Row } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 import CampsiteDetail from '../featuures/campsites/CampsiteDetail';
+
+const CampsiteDetailPage = () => {
+
+};
+
+export default CampsiteDetailPage;
